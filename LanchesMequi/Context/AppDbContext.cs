ba0484a7c1b3af54152b5c9e9c13f19f4f7222ca
Alexandre Dio <alexandre.dio@gmail.com>
2022-11-26 18:45:28 -0300

    Criação da nova entidade CarrinhoComprasItem e geração do migration

--- LanchesMequi/Context/AppDbContext.cs
+++ LanchesMequi/Context/AppDbContext.cs
@@ -12,5 +12,6 @@ namespace LanchesMequi.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
     }
 }
