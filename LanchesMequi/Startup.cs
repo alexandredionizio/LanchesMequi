6e0266eda288cd06c89c59037d73face96ce92c1
Alexandre Dio <alexandre.dio@gmail.com>
2022-11-26 19:55:04 -0300

    Implementação do carrinho de compras

--- LanchesMequi/Startup.cs
+++ LanchesMequi/Startup.cs
@@ -21,8 +21,12 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +47,7 @@ public class Startup
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthorization();
+        app.UseSession();
 
         app.UseEndpoints(endpoints =>
         {
