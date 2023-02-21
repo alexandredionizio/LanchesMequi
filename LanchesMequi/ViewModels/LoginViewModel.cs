18d9bbc4cb1a97cf9ab063cb4fd91d7f5ede6eef
Alexandre Dio <alexandre.dio@gmail.com>
2023-02-21 20:49:57 -0300

    Implementando o login - 2 parte

---
+++ LanchesMequi/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMequi.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o usuário")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe s senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
