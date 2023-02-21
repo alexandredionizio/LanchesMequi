c261170100ed823bdf7d5f466b8c66c623bef16b
Alexandre Dio <alexandre.dio@gmail.com>
2023-02-21 16:39:00 -0300

    Criando o controlador checkout

---
+++ LanchesMequi/Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMequi.Models;
+using LanchesMequi.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMequi.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, 
+            CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) 
+        {
+            return View();
+        }
+    }
+}
