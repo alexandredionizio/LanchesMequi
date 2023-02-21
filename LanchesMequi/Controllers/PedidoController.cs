052d4d2d5fd2546f6ecd95c7f204d36bcd068967
Alexandre Dio <alexandre.dio@gmail.com>
2023-02-21 19:05:27 -0300

    Criando o método Action Checkout (HttpPost)

--- LanchesMequi/Controllers/PedidoController.cs
+++ LanchesMequi/Controllers/PedidoController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMequi.Models;
 using LanchesMequi.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LanchesMequi.Controllers
 {
@@ -25,7 +26,47 @@ namespace LanchesMequi.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) 
         {
-            return View();
+            int totalIntensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obtém os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems= items;
+
+            //verifica se existem itens do pedido
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0) 
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche?");
+            }
+
+            //calcula o total de itens e o total do pedido
+            foreach (var item in items)
+            {
+                totalIntensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalIntensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido: ";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
     }
 }
