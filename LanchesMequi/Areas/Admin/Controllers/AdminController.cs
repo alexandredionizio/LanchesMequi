983709dc968ac9c2aba18ebe0abb8c2332424270
Alexandre Dio <alexandre.dio@gmail.com>
2023-02-26 17:10:51 -0300

    Criando os controladores e views para gerenciar lanches, categorias e pedidos pt1

--- LanchesMequi/Areas/Admin/Controllers/AdminController.cs
+++ LanchesMequi/Areas/Admin/Controllers/AdminController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LanchesMequi.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
