e15f616dfdc3f7fb1e88f4c200225a13d232cd11
Alexandre Dio <alexandre.dio@gmail.com>
2024-07-24 15:50:22 -0300

    feat: Migrando o código da classe Startup.cs para a classe Program.cs para atender ao Visual Studio 2022 a partir da versão .Net 6.

--- LanchesMequi/Program.cs
+++ LanchesMequi/Program.cs
@@ -1,46 +1,117 @@
-using Microsoft.AspNetCore.Hosting;
+using LanchesMequi.Areas.Admin.Servicos;
+using LanchesMequi.Context;
+using LanchesMequi.Models;
+using LanchesMequi.Repositories;
+using LanchesMequi.Repositories.Interfaces;
+using LanchesMequi.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
 
-namespace LanchesMequi;
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration
+    .GetSection("ConfigurationPastaImagens"));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<RelatorioVendasService>();
+builder.Services.AddScoped<GraficoVendasService>();
+
+//registrar a politica para autorização baseada nas roles
+builder.Services.AddAuthorization(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    options.AddPolicy("Admin",
+        politica =>
+        {
+            politica.RequireRole("Admin");
+        });
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+//Por isso que criamos o método GetCarrinho como estático na pasta Models, para podermos invocar esse método
+//sem ter uma instância da classe e já obter a partir da sessão um carrinho de compras com o 
+//contexto e com uma lista de itens.
+//AddScoped permite que seja criada uma instância a cada request.
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseStaticFiles();
+app.UseRouting();
 
 
-//var builder = WebApplication.CreateBuilder(args);
+////cria os perfis => comentado pois agora como é feito pelo program.cs, tivemos que criar o método abaixo CriarPerfisUsuarios()
+//seedUserRoleInitial.SeedRoles();
+////criar os usuários e atributos ao perfil
+//seedUserRoleInitial.SeedUsers();
 
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
+CriarPerfisUsuarios(app);
 
-//var app = builder.Build();
+app.UseSession();
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+app.UseAuthentication();
+app.UseAuthorization();
 
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
-//app.UseRouting();
+    endpoints.MapControllerRoute(
+        name: "categoriaFiltro",
+        pattern: "Lanche/{action}/{categoria?}",
+        defaults: new { Controller = "Lanche", Action = "List" });
 
-//app.UseAuthorization();
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
 
-//app.Run();
+void CriarPerfisUsuarios(WebApplication app) {
+    var scopedFactory = app.Services.GetService <IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedUsers();
+        service.SeedRoles();
+    }
+}
